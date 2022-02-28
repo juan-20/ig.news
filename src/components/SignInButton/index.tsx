c6025e3beaaba8dde5b6b88599baab5b36801948
Juan Andrade <49418565+juan-20@users.noreply.github.com>
2022-02-28 14:56:46 -0300

    login done :heavy_check_mark:

--- src/components/SignInButton/index.tsx
+++ src/components/SignInButton/index.tsx
@@ -1,25 +1,32 @@
 import styles from './styles.module.scss'
 import { FaGithub } from "react-icons/fa"
 import { FiX } from "react-icons/fi"
+import { signIn, signOut, useSession } from 'next-auth/client'
 
 function SignInButton() {
 
-  const isUserLoggedIn = false;
+  const [session] = useSession();
 
-  return isUserLoggedIn ? (
+  console.log(session)
+
+  return session ? (
     <button
       type='button'
-      className={styles.signInButton}>
-      <FaGithub color='#eba417' />
-      Sign in with Github
+      className={styles.signInButton}
+      onClick={() => signOut()}
+    >
+      <FaGithub color='#04d361' />
+      {session.user.name}
+      <FiX color='#737380' className={styles.closeIcon} />
     </button>
   ) : (
     <button
       type='button'
-      className={styles.signInButton}>
-      <FaGithub color='#04d361' />
-      Juan-20
-      <FiX color='#737380' className={styles.closeIcon} />
+      className={styles.signInButton}
+      onClick={() => signIn('github')}
+    >
+      <FaGithub color='#eba417' />
+      Sign in with Github
     </button>
   )
 }
